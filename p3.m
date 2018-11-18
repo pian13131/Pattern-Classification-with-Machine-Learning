b1bfc83a8bbead0120dd745e034604143ace6cd8
Lyu <jialun13131@hotmail.com>
2018-11-17 20:35:39 -0800

    Add the p3

--- p3.m
+++ p3.m
@@ -2,59 +2,63 @@ clc;
 clear;
 
 load('TrainingSamplesDCT_subsets_8.mat');
-load('Prior_1.mat');
-load('Prior_2.mat');
 load('Alpha.mat');
+load('Prior_1.mat');
+
+
 asz = size(alpha);
 asz = asz(2);
 
-cov_D1_BG = cov(D1_BG);
-cov_D1_FG = cov(D1_FG);
-
-mu0_s1_BG = zeros(1, 64);
-mu0_s1_BG(1) = 3;
-mu0_s1_FG = zeros(1, 64);
-mu0_s1_FG(1) = 1;
-
-sz = size(D1_BG);
-szBG_D1 = sz(1);
-sz = size(D1_FG);
-szFG_D1 = sz(1);
-PYiBG_D1 = szBG_D1/(szBG_D1 + szFG_D1);
-PYiFG_D2 = 1 - PYiBG_D1;
-
-Sigma0 = zeros(64, 64, asz);
+Sigma0 = zeros(64, 64);
 for i = 1:64
-   for j = 1:asz
-       Sigma0(i, i, j) = alpha(j)*W0(i);
-   end
+    Sigma0(i, i) = W0(i);
 end
 
-
-% calculate the 64 dimensional features for every point in origin image,
-% for the 8 dimensional feature just pick out them from 64 dimensional
-% feature vectors with the pickID
-A = imread("cheetah.bmp");
-A = im2double(A);
-Apad = padarray(A,[7 7],'symmetric','post'); % Pad the origin image
-load('Zig-Zag Pattern.txt');
-zigzag = Zig_Zag_Pattern + 1;
-
-correctImg = imread('cheetah_mask.bmp');
-calculatedImg64 = zeros(255, 270);
-calculatedImg8 = zeros(255, 270);
-
-fun=inline('normpdf(x, mu, Sigma0(1)(1)(1))','x');
-Isim=quad(fun,0,1) 
-
-for i = 1 : 255
-    for j = 1 : 270
-        window = dct2(Apad(i:i+7, j:j+7), 8, 8);
-        feature64(zigzag) = window;
-        dBG64 = (feature64 - meanBG64)/(covBG64)*(feature64 - meanBG64)'; % Calculate the d value in the BDR function
-        dFG64 = (feature64 - meanFG64)/(covFG64)*(feature64 - meanFG64)';
-        if (dBG64 + aBG64 > dFG64 + aFG64)
-            calculatedImg64(i, j) = 1;
-        end
-    end
+e = zeros(3,9);
+for i = 1:9
+   [e(1,i), e(2,i), e(3,i)] = BDR(D1_BG, D1_FG, mu0_BG, mu0_FG, Sigma0*alpha(i));
 end
+plot(alpha,e(1,:),alpha,e(2,:),alpha,e(3,:));
+set(gca, 'XScale', 'log');
+legend('Bayes','MAP', 'ML')
+xlabel('Alpha');
+ylabel('Error');
+hold on;
+title('D1 with Strategy 1');
+
+for i = 1:9
+   [e(1,i), e(2,i), e(3,i)] = BDR(D2_BG, D2_FG, mu0_BG, mu0_FG, Sigma0*alpha(i));
+end
+plot(alpha,e(1,:),alpha,e(2,:),alpha,e(3,:));
+set(gca, 'XScale', 'log');
+legend('Bayes','MAP', 'ML')
+xlabel('Alpha');
+ylabel('Error');
+hold on;
+title('D2 with Strategy 1');
+
+for i = 1:9
+   [e(1,i), e(2,i), e(3,i)] = BDR(D3_BG, D3_FG, mu0_BG, mu0_FG, Sigma0*alpha(i));
+end
+plot(alpha,e(1,:),alpha,e(2,:),alpha,e(3,:));
+set(gca, 'XScale', 'log');
+legend('Bayes','MAP', 'ML')
+xlabel('Alpha');
+ylabel('Error');
+hold on;
+title('D3 with Strategy 1');
+
+for i = 1:9
+   [e(1,i), e(2,i), e(3,i)] = BDR(D4_BG, D4_FG, mu0_BG, mu0_FG, Sigma0*alpha(i));
+end
+plot(alpha,e(1,:),alpha,e(2,:),alpha,e(3,:));
+set(gca, 'XScale', 'log');
+legend('Bayes','MAP', 'ML')
+xlabel('Alpha');
+ylabel('Error');
+hold on;
+title('D4 with Strategy 1');
+
+
+load('Prior_2.mat');
+
