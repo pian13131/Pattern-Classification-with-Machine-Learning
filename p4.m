4cbeb1a4b93767a4e3e8e1e230079f7e4e68434a
Lyu Jialun <lyjialun@its-blb-mac04.ucsd.edu>
2018-12-04 09:59:57 -0800

    start plot

--- p4.m
+++ p4.m
@@ -1,34 +1,35 @@
 clc;
 clear;
 init();
+global D_BG D_FG py_BG py_FG;
 %-------- Part 1 --------%
 D = [1 2 4 8 16 24 32 40 48 56 64];
 
-e1 = zeros(5,5,11);
-P_BG55 = zeros(5, 11, 255, 260);
-P_FG55 = zeros(5, 11, 255, 260);
-for j=1:5
-    [pi_BG, mu_BG, Sigma_BG] = calh(D_BG,64,8);
-    [pi_FG, mu_FG, Sigma_FG] = calh(D_FG,64,8);
-    for i=1:11
-        d = D(i);
-        P_BG55(j,i,:,:) = py_BG * callike(pi_BG, mu_BG(:,1:d), Sigma_BG(:,1:d,1:d));
-        P_FG55(j,i,:,:) = py_FG * callike(pi_FG, mu_FG(:,1:d), Sigma_FG(:,1:d,1:d));
-    end
-end
-for i=1:5
-   for j=1:5
-      for k=
-      e1(i,j,:) =  
-   end
-end
-for i = 1:5
-    figure;
-    plot(D,e1((i-1)*5+1:i*5,:));
-    xlabel('Dimension')
-    ylabel('Error')
-    title(['Error vs Dimension FG',mat2str(i)]);
-end
+% e1 = zeros(5,5,11);
+% P_BG55 = zeros(5, 11, 255, 260);
+% P_FG55 = zeros(5, 11, 255, 260);
+% for j=1:5
+%     [pi_BG, mu_BG, Sigma_BG] = calh(D_BG,64,8);
+%     [pi_FG, mu_FG, Sigma_FG] = calh(D_FG,64,8);
+%     for i=1:11
+%         d = D(i);
+%         P_BG55(j,i,:,:) = py_BG * callike(pi_BG, mu_BG(:,1:d), Sigma_BG(:,1:d,1:d));
+%         P_FG55(j,i,:,:) = py_FG * callike(pi_FG, mu_FG(:,1:d), Sigma_FG(:,1:d,1:d));
+%     end
+% end
+% for i=1:5
+%    for j=1:5
+%       for k=
+%       e1(i,j,:) =  
+%    end
+% end
+% for i = 1:5
+%     figure;
+%     plot(D,e1((i-1)*5+1:i*5,:));
+%     xlabel('Dimension')
+%     ylabel('Error')
+%     title(['Error vs Dimension FG',mat2str(i)]);
+% end
 
 %-------- Part 2 --------%
 C = [1 2 4 8 16 32];
